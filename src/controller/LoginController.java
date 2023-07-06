57bfad0a5781d78657c9534a5b826f046b9c208c
ethanmaxwellharris <ethan.harriss@gmail.com>
2023-07-06 17:32:03 -0600

    Initial Commit

---
+++ src/controller/LoginController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    public Label welcomeBackLabel;
+    public Label welcomeBackSubTextLabel;
+    public Label emailLabel;
+    public Label passwordLabel;
+    public Button signInButtonLabel;
+    public Label languagePreferenceLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+         System.out.println("LoginController is Initialized!");
+
+    }
+
+    public void onButtonAction(ActionEvent actionEvent) {
+        System.out.println("Sign On Button Clicked!");
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+        stage.setTitle("Login");
+        stage.setScene(new Scene(root, 400, 500)); //width then height
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
