a8eb5cd6334dd19fb800cb60981018f74430bf26
ethanmaxwellharris <ethan.harriss@gmail.com>
2023-07-08 13:43:55 -0600

    Began updating the database connections for each of the DAO and corresponding models

--- src/dao/DBFirstLevelDivisions.java
+++ src/dao/DBFirstLevelDivisions.java
@@ -1,4 +1,31 @@
 package dao;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.FirstLevelDivisions;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DBFirstLevelDivisions {
+    public static ObservableList<FirstLevelDivisions> getAllFirstLevelDivisions() {
+        ObservableList<FirstLevelDivisions> fList = FXCollections.observableArrayList();
+
+        try {
+            String sql = "SELECT * FROM firstleveldivisions";
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                int division_id = rs.getInt("Division ID");
+                String division = rs.getString("Division");
+                int country_id = rs.getInt("Country ID");
+                FirstLevelDivisions f = new FirstLevelDivisions(division_id, division, country_id);
+                fList.add(f);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return fList;
+    }
 }
--- src/dao/DBUsers.java
+++ src/dao/DBUsers.java
@@ -1,4 +1,31 @@
 package dao;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Users;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DBUsers {
+    public static ObservableList<Users> getAllUsers() {
+        ObservableList<Users> uList = FXCollections.observableArrayList();
+
+        try {
+            String sql = "SELECT * FROM users";
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                int userId = rs.getInt("User ID");
+                String userName = rs.getString("Name");
+                String password = rs.getString("Password");
+                Users u = new Users(userId, userName, password);
+                uList.add(u);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return uList;
+    }
 }
